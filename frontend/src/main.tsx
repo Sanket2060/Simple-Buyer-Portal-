8f7b468d70391e358ddd0b89384d1fa7ea089ff3
Sanket <sanketkarki2060@gmail.com>
2026-03-31 16:09:16 +0545

    feat: install tailwindcss and add react router dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "./App.css";
+import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import Property from "./pages/Property.tsx";
+import Favourites from "./pages/Property.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { BrowserRouter, Route, Routes } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/property/:id" element={<Property />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
     <App />
   </StrictMode>,
-)
+);
